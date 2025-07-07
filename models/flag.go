6b7c63222ef29162cdb7bddd529c151c2665ee62
alirezasaharkhiz <alireza.saharkhiz@outlook.com>
2025-07-07 23:34:39 +0330

    Add err Update flag model

--- models/flag.go
+++ models/flag.go
@@ -1,12 +1,10 @@
 package models
 
-import (
-	"gorm.io/gorm"
-)
+import "gorm.io/gorm"
 
 type Flag struct {
 	gorm.Model
 	Name         string       `gorm:"uniqueIndex;not null"`
 	Enabled      bool         `gorm:"default:false"`
-	Dependencies []Dependency `gorm:"foreignKey:FlagID;constraint:OnDelete:CASCADE"`
+	Dependencies []Dependency `gorm:"foreignKey:FlagID;constraint:OnDelete:CASCADE" json:"dependencies,omitempty"`
 }
