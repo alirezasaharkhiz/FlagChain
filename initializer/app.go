3e12e28d4164b3576a8b4c6c15d46068ab1e9901
alirezasaharkhiz <alireza.saharkhiz@outlook.com>
2025-07-07 23:31:56 +0330

    Fix app configuration

--- initializer/app.go
+++ initializer/app.go
@@ -1,13 +1,15 @@
 package initializer
 
 import (
+	"errors"
+	"github.com/alirezasaharkhiz/FlagChain/services"
+	"log"
+
 	"github.com/alirezasaharkhiz/FlagChain/config"
 	"github.com/alirezasaharkhiz/FlagChain/controllers"
 	"github.com/alirezasaharkhiz/FlagChain/middlewares"
 	"github.com/alirezasaharkhiz/FlagChain/repositories"
 	"github.com/alirezasaharkhiz/FlagChain/routes"
-	"github.com/alirezasaharkhiz/FlagChain/services"
-	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-migrate/migrate/v4"
@@ -24,33 +26,34 @@ type App struct {
 func NewApp() *App {
 	cfg := config.LoadConfig()
 
-	// اجرای مهاجرت‌ها
+	// Run migrations
 	dsn := "mysql://" + cfg.DBUser + ":" + cfg.DBPassword + "@tcp(" + cfg.DBHost + ")/" + cfg.DBName + "?multiStatements=true"
 	m, err := migrate.New("file://"+cfg.MigrationsDir, dsn)
 	if err != nil {
 		log.Fatalf("Migration setup failed: %v", err)
 	}
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		log.Fatalf("Migration up failed: %v", err)
+	if err := m.Up(); err != nil {
+		if !errors.Is(err, migrate.ErrNoChange) {
+			log.Fatalf("Migration up failed: %v", err)
+		}
 	}
 
-	// اتصال GORM
+	// Connect to DB with GORM
 	gormDSN := cfg.DBUser + ":" + cfg.DBPassword + "@tcp(" + cfg.DBHost + ")/" + cfg.DBName + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(gormDSN), &gorm.Config{})
 	if err != nil {
-		log.Fatalf("Failed to connect database: %v", err)
+		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
-	// مقداردهی اولیه مخازن داده
+	// Initialize repositories, services, controllers
 	flagRepo := repositories.NewFlagRepository(db)
 	depRepo := repositories.NewDependencyRepository(db)
 	auditRepo := repositories.NewAuditRepository(db)
 
-	// سرویس و کنترلر
 	service := services.NewFeatureFlagService(flagRepo, depRepo, auditRepo)
 	controller := controllers.NewFeatureFlagController(service)
 
-	// راه‌اندازی Gin و روتر
+	// Setup router
 	router := gin.Default()
 	router.Use(middlewares.ErrorHandler)
 	routes.RegisterFlagRoutes(router, controller)
@@ -60,5 +63,7 @@ func NewApp() *App {
 
 func (a *App) Run() {
 	cfg := config.LoadConfig()
-	a.Router.Run(cfg.ServerPort)
+	if err := a.Router.Run(cfg.ServerPort); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
