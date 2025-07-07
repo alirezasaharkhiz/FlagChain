9e1bda94642fe92d300ecddc4e90e5353ac652d8
alirezasaharkhiz <alireza.saharkhiz@outlook.com>
2025-07-07 23:33:08 +0330

    Add new functions to GormDependencyRepository

--- repositories/dependency_repository.go
+++ repositories/dependency_repository.go
@@ -11,11 +11,22 @@ func NewDependencyRepository(db *gorm.DB) DependencyRepository { return &GormDep
 func (r *GormDependencyRepository) Add(dep *models.Dependency) error {
 	return r.db.Create(dep).Error
 }
-func (r *GormDependencyRepository) ListFor(flagID uint) ([]models.Dependency, error) {
+func (r *GormDependencyRepository) ListWhere(condition string, args ...interface{}) ([]models.Dependency, error) {
 	var deps []models.Dependency
-	err := r.db.Where("flag_id = ?", flagID).Find(&deps).Error
+	err := r.db.Where(condition, args...).Find(&deps).Error
 	return deps, err
 }
 func (r *GormDependencyRepository) RemoveAllFor(flagID uint) error {
 	return r.db.Where("flag_id = ?", flagID).Delete(&models.Dependency{}).Error
+}
+
+func (r *GormDependencyRepository) Exists(flagID, dependsOnID uint) (bool, error) {
+	var count int64
+	err := r.db.Model(&models.Dependency{}).
+		Where("flag_id = ? AND depends_on_id = ?", flagID, dependsOnID).
+		Count(&count).Error
+	if err != nil {
+		return false, err
+	}
+	return count > 0, nil
 }
